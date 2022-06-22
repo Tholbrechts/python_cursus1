e3100c8e9c42da74b2193013b7877ee2c07f275d
Tholbrechts <thomasholbrechts@hotmail.com>
2022-06-22 10:11:58 +0200

    videos sur les fonction

---
+++ fonction.py
@@ -0,0 +1,15 @@
+
+def max(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+first_value = int(input("entrer une valeur"))
+second_value = int(input("entrer une valeur"))
+max_value = max(first_value, second_value)
+print("la valeur haute est ", max_value)
+
+
+
+
