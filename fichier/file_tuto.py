b83f40388f5f32b4dd9611e9b7179235013c8faa
Tholbrechts <thomasholbrechts@hotmail.com>
2022-06-24 13:22:45 +0200

    update

--- fichier/file_tuto.py
+++ fichier/file_tuto.py
@@ -2,8 +2,8 @@
 import os
 import random
 
-if os.path.exists("meals.txt"):
-    with open("meals.txt", "r+") as file:
+if os.path.exists("../mealsapp/meals.txt"):
+    with open("../mealsapp/meals.txt", "r+") as file:
         meals_liste = file.readlines()
         meal_random_choice = random.choice((meals_liste))
         print(meal_random_choice)
